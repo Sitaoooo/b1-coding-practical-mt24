c469aef63b3cee0870ce0d455e7f420762f43d4e
sitaoooo <sitao.huang@magd.ox.ac.uk>
2024-10-16 23:43:53 +0100

    modify jupyter notebook to complete the simulation, testing with Kp and Kd

---
+++ uuv_mission/__init__.py
@@ -0,0 +1,4 @@
+from .controller import Controller
+from .dynamic import Submarine, ClosedLoop, Mission
+
+__all__ = ['Controller', 'Submarine', 'ClosedLoop', 'Mission']
--- uuv_mission/controller.py
+++ uuv_mission/controller.py
@@ -1,4 +1,4 @@
-class controller:
+class Controller:
     """
     This class implement using a PD feedback controller
     attribute: 
